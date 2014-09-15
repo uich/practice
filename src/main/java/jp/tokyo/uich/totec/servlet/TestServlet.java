ad5b98caa80d237a89cd3a5fb00c34a0beca69cc
morimichi_yuichi <morimichi_yuichi@cyberagent.co.jp>
2014-09-15 17:15:04 +0900

    add redis connection.

--- src/main/java/jp/tokyo/uich/totec/servlet/TestServlet.java
+++ src/main/java/jp/tokyo/uich/totec/servlet/TestServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 @SuppressWarnings("javadoc")
 @WebServlet(name = "TestServlet", urlPatterns = "/test")
 public class TestServlet extends HttpServlet {
@@ -16,6 +20,25 @@ public class TestServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("HELLO!");
+		//		req.setCharacterEncoding("UTF-8");
+		//		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/plain; charset=UTF-8");
+		String value = getValue(req.getParameter("key"));
+		//		String value = "テストだよー";
+		resp.getWriter().print(value);
+	}
+
+	private String getValue(String key) {
+		JedisPoolConfig config = new JedisPoolConfig();
+		//		JedisPool pool = new JedisPool(config, "54.68.163.231", 6379);
+		JedisPool pool = new JedisPool(config, "localhost", 6379);
+
+		Jedis jedis = pool.getResource();
+		try {
+			return jedis.get(key);
+		} finally {
+			pool.returnResource(jedis);
+			pool.destroy();
+		}
 	}
 }
