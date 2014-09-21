1334e6b7ab902f7cb72389e9136fdffe1b0ebe3d
morimichi_yuichi <morimichi_yuichi@cyberagent.co.jp>
2014-09-22 01:43:32 +0900

    調整

--- src/main/java/jp/tokyo/uich/totec/servlet/TestServlet.java
+++ src/main/java/jp/tokyo/uich/totec/servlet/TestServlet.java
@@ -12,6 +12,12 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+/**
+ * 
+ * TODO: 要JavaDocコメント
+ * 
+ * @author a13076
+ */
 @SuppressWarnings("javadoc")
 @WebServlet(name = "TestServlet", urlPatterns = "/test")
 public class TestServlet extends HttpServlet {
@@ -30,8 +36,8 @@ public class TestServlet extends HttpServlet {
 
 	private String getValue(String key) {
 		JedisPoolConfig config = new JedisPoolConfig();
-		//		JedisPool pool = new JedisPool(config, "54.68.163.231", 6379);
-		JedisPool pool = new JedisPool(config, "localhost", 6379);
+		JedisPool pool = new JedisPool(config, "54.68.163.231", 6379);
+		//		JedisPool pool = new JedisPool(config, "localhost", 6379);
 
 		Jedis jedis = pool.getResource();
 		try {
